d935ae263767298565edbd17f304f3a2725a3107
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-12-16 00:07:51 +0900

    Add test

---
+++ test/mrb_mrbgem_template_test.rb
@@ -0,0 +1,59 @@
+##
+## MrbgemTemplate Test
+##
+
+assert("MrbgemTemplate#*_data") do
+  params = {
+    :mrbgem_name    => 'mruby-sample',
+    :license        => 'MIT',
+    :github_user    => 'matsumoto-r',
+    :mrbgem_prefix  => '..',
+    :class_name     => 'Sample',
+    :author         => 'MATSUMOTO Ryosuke',
+  }
+  
+  c = MrbgemTemplate.new params
+  assert_equal(c.src_c_data, c.src_c_data_init)
+  assert_equal(c.src_h_data, c.src_h_data_init)
+  assert_equal(c.mrblib_data, c.mrblib_data_init)
+  assert_equal(c.rake_data, c.rake_data_init)
+  assert_equal(c.mgem_data, c.mgem_data_init)
+  assert_equal(c.test_data, c.test_data_init)
+  assert_equal(c.license_data, c.license_data_init)
+  assert_equal(c.readme_data, c.readme_data_init)
+  assert_equal(c.travis_ci_data, c.travis_ci_data_init)
+  assert_equal(c.travis_build_config_data, c.travis_build_config_data_init)
+end
+
+assert("MrbgemTemplate#*_data=") do
+  params = {
+    :mrbgem_name    => 'mruby-sample',
+    :license        => 'MIT',
+    :github_user    => 'matsumoto-r',
+    :mrbgem_prefix  => '..',
+    :class_name     => 'Sample',
+    :author         => 'MATSUMOTO Ryosuke',
+  }
+  
+  c = MrbgemTemplate.new params
+  c.src_c_data = "aaa"
+  assert_equal("aaa", c.src_c_data)
+  c.src_h_data = "bbb"
+  assert_equal("bbb", c.src_h_data)
+  c.mrblib_data = "ccc"
+  assert_equal("ccc", c.mrblib_data)
+  c.rake_data = "rake"
+  assert_equal("rake", c.rake_data)
+  c.mgem_data = "mgem"
+  assert_equal("mgem", c.mgem_data)
+  c.test_data = "test"
+  assert_equal("test", c.test_data)
+  c.license_data = "license"
+  assert_equal("license", c.license_data)
+  c.readme_data = "readme"
+  assert_equal("readme", c.readme_data)
+  c.travis_ci_data = "ci"
+  assert_equal("ci", c.travis_ci_data)
+  c.travis_build_config_data = "build"
+  assert_equal("build", c.travis_build_config_data)
+end
