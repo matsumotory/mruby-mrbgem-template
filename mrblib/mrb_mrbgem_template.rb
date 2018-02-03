bdfaaabb15c78159eeac5861ac224cd48b83d213
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-02-04 03:39:09 +0900

    Update default MRUBY_VERSION to 1.4.0
    ref: https://github.com/mruby/mruby/tree/1.4.0

--- mrblib/mrb_mrbgem_template.rb
+++ mrblib/mrb_mrbgem_template.rb
@@ -213,7 +213,7 @@ DATA
   def builder_data_init
     <<DATA
 MRUBY_CONFIG=File.expand_path(ENV["MRUBY_CONFIG"] || ".travis_build_config.rb")
-MRUBY_VERSION=ENV["MRUBY_VERSION"] || "1.3.0"
+MRUBY_VERSION=ENV["MRUBY_VERSION"] || "1.4.0"
 
 file :mruby do
   sh "git clone --depth=1 git://github.com/mruby/mruby.git"
