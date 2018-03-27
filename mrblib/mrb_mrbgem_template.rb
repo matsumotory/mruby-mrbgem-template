29a22c7789cbf400124fabdcb802047e71310eb7
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-03-27 21:06:47 +0900

    Add DEBUG Options to .travis_build_config.rb
    Run `DEBUG=true rake compile` and then mrdb command is generated for debugging.

--- mrblib/mrb_mrbgem_template.rb
+++ mrblib/mrb_mrbgem_template.rb
@@ -322,6 +322,12 @@ MRuby::Build.new do |conf|
   conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__))
   conf.enable_test
+
+  if ENV['DEBUG'] == 'true'
+    conf.enable_debug
+    conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
+    conf.gem core: 'mruby-bin-debugger'
+  end
 end
 DATA
   end
