6ad6f20be94ea21ef9dfc356bda15fd81f98280e
tascript <wataruscript8611@gmail.com>
2021-10-16 23:57:12 +0900

    add work flow

---
+++ .actions_build_config.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+MRuby::Build.new do |conf|
+  toolchain :gcc
+  conf.gembox 'default'
+  conf.gem git: 'https://github.com/iij/mruby-io.git'
+  conf.gem git: 'https://github.com/iij/mruby-dir.git'
+  conf.gem '../mruby-mrbgem-template'
+end
